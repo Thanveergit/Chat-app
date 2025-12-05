e4a9d4a763025f15ba85b90c67e25676feb8fb9f
thanveer <thanveermohd95@gmail.com>
2025-12-05 19:03:29 +0530

    updation in axios baseUrl

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
      // baseURL:  "http://localhost:5001/api",
-     baseURL: "https://chat-app-9-81hl.onrender.com",
+     baseURL: "https://chat-app-9-81hl.onrender.com/api",
      withCredentials:true
 })
