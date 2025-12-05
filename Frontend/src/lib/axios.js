1e8c373bf4e27799d18266364425d0fb09e47fa3
thanveer <thanveermohd95@gmail.com>
2025-12-05 11:14:19 +0530

    added render url

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-     baseURL:  "http://localhost:5001/api",
+     // baseURL:  "http://localhost:5001/api",
+     baseURL: "https://chat-app-9-81hl.onrender.com",
      withCredentials:true
 })
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -2,7 +2,8 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client";
-const BASE_URL = import.meta.env.MODE === "development "?"http://localhost:5001":"/"
+// const BASE_URL = import.meta.env.MODE === "development "?"http://localhost:5001":"/"
+const BASE_URL = "https://chat-app-9-81hl.onrender.com"
 
 export const useAuthStore = create((set,get) => ({
   authUser: null,
